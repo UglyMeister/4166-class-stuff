76cc117f5c006cd553929f9284a6826014b08eb4
Fred York <fredwashere53@gmail.com>
2019-03-12 02:24:05 -0400

    exercise 6 creation

---
+++ Exercise 6/controls/index.js
@@ -0,0 +1,22 @@
+/* @nanajjar */
+
+var express = require('express');
+var app = module.exports = express();
+
+/* This controller handles general (root) request 
+ *  this route definition resposnds by sending a text response
+ */
+
+/*
+app.get('/', function (req, res) {
+  res.send('This is the Student Information home page');
+});
+*/
+
+//this is the home page route (root of the application)
+app.get('/', function (request, response) {
+    var path = process.cwd();
+
+    console.log('request for home page');
+    response.sendFile(path + '/views/index.html');
+});
